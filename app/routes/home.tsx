83c38434ec691a5e92853c0cb6d66e509cdcb5fa
Nanda Syahrasyad <nanda.s@hey.com>
2026-01-04 12:33:47 -0800

    update color scheme

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -20,7 +20,7 @@ export default function Home() {
 	useEffect(() => {
 		const calculateTime = () => {
 			const now = new Date();
-			const targetDate = new Date(now.getFullYear(), 2, 10, 13, 35); // March 10th at 13:35 (month is 0-indexed)
+			const targetDate = new Date(now.getFullYear(), 2, 10, 14, 35); // March 10th at 13:35 (month is 0-indexed)
 
 			// If March 10th 13:35 has passed this year, target next year
 			if (targetDate < now) {
@@ -59,26 +59,27 @@ export default function Home() {
 
 	return (
 		<main className="flex items-center justify-center h-screen">
-			<header className="fixed top-8 left-8 text-3xl font-bold">
-				<h1 className="flex flex-col leading-[0.8]">
-					<span>nan</span>
-					<span>ana</span>
-				</h1>
-			</header>
 			<div className="flex flex-col gap-4 items-center">
 				<div className="text-4xl font-medium flex">
-					<p>YVR</p>
+					<p
+						style={{
+							transform: "rotate(-20deg)",
+						}}
+					>
+						YVR
+					</p>
 					<svg
 						width="100"
 						viewBox="0 0 296 71"
 						fill="none"
 						xmlns="http://www.w3.org/2000/svg"
 						aria-hidden="true"
+						className="-ml-2"
 					>
 						<g clip-path="url(#clip0_3_228)">
 							<path
 								d="M-3.10351e-06 71C13.102 44.3623 83.0496 8.91537 132.922 2.62301C186.809 -4.29859 217.873 1.57428 281.27 31.1484C280.425 26.9535 279.368 23.5976 278.945 20.4514C278.311 16.8857 277.677 13.1103 277.889 9.54461C277.889 8.49589 279.791 6.60818 281.059 6.39843C282.327 6.18869 284.44 7.6569 284.862 8.70563C288.666 18.7734 292.047 28.8412 295.64 39.1187C296.696 42.4746 295.428 44.9916 291.836 46.0403C278.1 49.3962 264.364 52.9619 250.417 56.1081C247.247 56.7373 243.443 55.8983 239.217 55.8983C242.598 46.8793 250.206 47.928 255.7 45.6208C261.406 43.1039 267.534 41.6357 274.93 39.1187C257.39 27.373 239.639 20.0319 220.832 15.4175C151.941 -1.78164 88.544 12.481 30.0078 50.6547C22.4002 55.6886 15.4265 61.5615 8.03027 66.8051C5.70572 67.8538 3.59247 68.9026 -3.10351e-06 71Z"
-								fill="white"
+								fill="currentColor"
 							/>
 						</g>
 						<defs>
@@ -87,10 +88,16 @@ export default function Home() {
 							</clipPath>
 						</defs>
 					</svg>
-					<p>CDG</p>
+					<p
+						style={{
+							transform: "rotate(20deg)",
+						}}
+					>
+						CDG
+					</p>
 				</div>
-				<p className="flex flex-col md:flex-row text-7xl font-bold">
-					<span className="grid grid-cols-2 gap-4">
+				<p className="text-center flex flex-col items-center md:flex-row text-7xl font-bold">
+					<span className="flex gap-4">
 						<span>
 							{days}
 							<span className="text-4xl">d</span>
@@ -100,7 +107,7 @@ export default function Home() {
 							<span className="text-4xl">h</span>
 						</span>
 					</span>
-					<span className="grid grid-cols-2 gap-4">
+					<span className="flex gap-4 -mt-3 md:mt-0">
 						<span>
 							{minutes}
 							<span className="text-4xl">m</span>
@@ -111,6 +118,10 @@ export default function Home() {
 						</span>
 					</span>
 				</p>
+				<h1 className="flex flex-col leading-[0.8] text-3xl font-bold mt-20">
+					<span>nan</span>
+					<span>ana</span>
+				</h1>
 			</div>
 		</main>
 	);
